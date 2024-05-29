841193cb4d28e8c422b3b44815f4c421b3ad0ed8
Fran Lopez <franciscolopto@gmail.com>
2024-05-29 11:09:00 +0200

    create sign-up component

--- intranet/src/pods/sign-up/sign-up.component.tsx
+++ intranet/src/pods/sign-up/sign-up.component.tsx
@@ -1,19 +1,40 @@
+import { useState } from 'react';
 import { Button, Typography } from '@mui/material';
+
+import SignUpStepTwoComponent from './component/sing-up-step2.component';
 
 import * as classes from './sign-up.styles';
 
 export const SignUpComponent: React.FC = () => {
+  const [formData, setFormdata] = useState({
+    name: '',
+    address: '',
+    city: '',
+    phoneNumber: '',
+  });
+
+  const onTextFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormdata({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleNextClick = () => {
+    console.log(formData);
+  };
   return (
     <>
-      <Typography variant="h4" component="h1">
+      <Typography variant="h4" component="h1" className={classes.title}>
         Crea tu cuenta
+        <div className={classes.steps}>
+          <SignUpStepTwoComponent formData={formData} onTextFieldChange={onTextFieldChange} />
+        </div>
       </Typography>
-      <div className={classes.steps}>{/* Components for diferents estates of sign-in */}</div>
       <div className={classes.buttons}>
         <Button color="secondary" variant="contained">
           Anterior
         </Button>
-        <Button variant="contained">Siguiente</Button>
+        <Button variant="contained" onClick={handleNextClick}>
+          Siguiente
+        </Button>
       </div>
     </>
   );
