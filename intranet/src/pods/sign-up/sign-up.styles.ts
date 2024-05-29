ad509ef2ec5ba0347e343d2c50a52d754e25d8da
Fran Lopez <franciscolopto@gmail.com>
2024-05-29 11:21:24 +0200

    add padding in buttons, steps and center the title text

--- intranet/src/pods/sign-up/sign-up.styles.ts
+++ intranet/src/pods/sign-up/sign-up.styles.ts
@@ -1,12 +1,19 @@
+import { mainTheme } from '#core/theme/main.theme.js';
 import { css } from '@emotion/css';
 
 export const buttons = css`
   width: 100%;
   display: flex;
   justify-content: space-between;
+  padding-top: ${mainTheme.spacing(3.5)};
 `;
 
 export const steps = css`
   min-height: 400px;
   width: 100%;
+  padding-top: ${mainTheme.spacing(3.5)};
+`;
+
+export const title = css`
+  text-align: center;
 `;
